6f75a689b109b251b1126380569806b31851b0ad
Francisco Javier Bolívar Lupiáñez <fblupi@correo.ugr.es>
2016-10-20 15:12:47 +0200

    Arreglado valor por defecto MB closes #11

--- P1/BolivarFrancisco-p1/src/main/java/controlador/ControladorMotor.java
+++ P1/BolivarFrancisco-p1/src/main/java/controlador/ControladorMotor.java
@@ -15,77 +15,71 @@ public class ControladorMotor implements Serializable {
 	private final int APAGADO = 0;
 	private final int ENCENDIDO = 1;
 	private final int ACELERANDO = 2;
-	
-	private final String tTituloApagado = "Apagado";
-	private final String tTituloEncendido = "Encendido";
-	private final String tTituloAcelerando = "Acelerando";
-	
-	private final String cTituloApagado = "red";
-	private final String cTituloEncendido = "green";
-	private final String cTituloAcelerando = "blue";
-	
-	private final String tBtnOnOffApagado = "Encender";
-	private final String tBtnOnOffEncendido = "Apagar";
-	
-	private final String cBtnOnOffApagado = "green";
-	private final String cBtnOnOffEncendido = "red";
-	
-	private final String cBtnAcelerarApagado = "grey";
-	private final String cBtnAcelerarEncendido = "black";
+	private final String TEXTO_APAGADO = "Apagado";
+	private final String TEXTO_ENCENDIDO = "Encendido";
+	private final String TEXTO_ACELERANDO = "Acelerando";
+	private final String TEXTO_ENCENDER = "Encender";
+	private final String TEXTO_APAGAR = "Apagar";
+	private final String COLOR_ROJO = "red";
+	private final String COLOR_VERDE = "green";
+	private final String COLOR_AZUL = "blue";
+	private final String COLOR_GRIS = "grey";
+	private final String COLOR_NEGRO = "black";
 
 	private int estado = APAGADO;
     
 	@ManagedProperty(value = "#{textoTitulo}")
-	private String textoTitulo = tTituloApagado;
+	private String textoTitulo;
 
 	@ManagedProperty(value = "#{colorTitulo}")
-	private String colorTitulo = cTituloApagado;
+	private String colorTitulo;
     
 	@ManagedProperty(value = "#{textoBtnOnOff}")
-	private String textoBtnOnOff = tBtnOnOffApagado;
+	private String textoBtnOnOff;
     
 	@ManagedProperty(value = "#{colorBtnOnOff}")
-	private String colorBtnOnOff = cBtnOnOffApagado;
+	private String colorBtnOnOff;
     
 	@ManagedProperty(value = "#{colorBtnAcelerar}")
-	private String colorBtnAcelerar = cBtnAcelerarApagado;
+	private String colorBtnAcelerar;
 	
 	private void encender() {
-		textoTitulo = tTituloEncendido;
-		colorTitulo = cTituloEncendido;
-		textoBtnOnOff = tBtnOnOffEncendido;
-		colorBtnOnOff = cBtnOnOffEncendido;
-		colorBtnAcelerar = cBtnAcelerarEncendido;
+		estado = ENCENDIDO;
+		textoTitulo = TEXTO_ENCENDIDO;
+		colorTitulo = COLOR_VERDE;
+		textoBtnOnOff = TEXTO_APAGAR;
+		colorBtnOnOff = COLOR_ROJO;
+		colorBtnAcelerar = COLOR_NEGRO;
 	}
 	
 	private void apagar() {
-		textoTitulo = tTituloApagado;
-		colorTitulo = cTituloApagado;
-		textoBtnOnOff = tBtnOnOffApagado;
-		colorBtnOnOff = cBtnOnOffApagado;
-		colorBtnAcelerar = cBtnAcelerarApagado;
+		estado = APAGADO;
+		textoTitulo = TEXTO_APAGADO;
+		colorTitulo = COLOR_ROJO;
+		textoBtnOnOff = TEXTO_ENCENDER;
+		colorBtnOnOff = COLOR_VERDE;
+		colorBtnAcelerar = COLOR_GRIS;
 	}
 
 	private void acelerar() {
-		textoTitulo = tTituloAcelerando;
-		colorTitulo = cTituloAcelerando;
+		estado = ACELERANDO;
+		textoTitulo = TEXTO_ACELERANDO;
+		colorTitulo = COLOR_AZUL;
 	}
 	
 	public void actualizarBtnOnOff(ActionEvent e) {
 		switch (estado) {
 		case APAGADO:
 			encender();
-			estado = ENCENDIDO;
 			break;
 		case ENCENDIDO:
 			apagar();
-			estado = APAGADO;
 			break;
 		case ACELERANDO:
 			apagar();
-			estado = APAGADO;
 			break;
 		default:
+			// idle
 			break;
 		}
 	}
@@ -93,22 +87,26 @@ public class ControladorMotor implements Serializable {
 	public void actualizarBtnAcelerar(ActionEvent e) {
 		switch (estado) {
 		case APAGADO:
+			// idle
 			break;
 		case ENCENDIDO:
 			acelerar();
-			estado = ACELERANDO;
 			break;
 		case ACELERANDO:
 			acelerar();
-			estado = ACELERANDO;
 			break;
 		default:
+			// idle
 			break;
 		}
 	}
 	
 	public String getTextoTitulo() {
-		return textoTitulo;
+		if (textoTitulo != null) {
+			return textoTitulo;
+		} else {
+			return TEXTO_APAGADO;
+		}
 	}
 
 	public void setTextoTitulo(String textoTitulo) {
@@ -116,7 +114,11 @@ public class ControladorMotor implements Serializable {
 	}
 
 	public String getColorTitulo() {
-		return colorTitulo;
+		if (colorTitulo != null) {
+			return colorTitulo;
+		} else {
+			return COLOR_ROJO;
+		}
 	}
 
 	public void setColorTitulo(String colorTitulo) {
@@ -124,7 +126,11 @@ public class ControladorMotor implements Serializable {
 	}
 
 	public String getTextoBtnOnOff() {
-		return textoBtnOnOff;
+		if (textoBtnOnOff != null) {
+			return textoBtnOnOff;
+		} else {
+			return TEXTO_ENCENDER;
+		}
 	}
 
 	public void setTextoBtnOnOff(String textoBtnOnOff) {
@@ -132,7 +138,11 @@ public class ControladorMotor implements Serializable {
 	}
 
 	public String getColorBtnOnOff() {
-		return colorBtnOnOff;
+		if (colorBtnOnOff != null) {
+			return colorBtnOnOff;
+		} else {
+			return COLOR_VERDE;
+		}
 	}
 
 	public void setColorBtnOnOff(String colorBtnOnOff) {
@@ -140,7 +150,11 @@ public class ControladorMotor implements Serializable {
 	}
 	
 	public String getColorBtnAcelerar() {
-		return colorBtnAcelerar;
+		if (colorBtnAcelerar != null) {
+			return colorBtnAcelerar;
+		} else {
+			return COLOR_GRIS;
+		}
 	}
 
 	public void setColorBtnAcelerar(String colorBtnAcelerar) {
